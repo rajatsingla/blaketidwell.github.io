ec864f5ffeb727883565650bc095ee0d1ac2496e
Blake Tidwell <batidwell@gmail.com>
2014-03-23 21:37:07 -0700

    Fixes Middleman build

--- config.rb
+++ config.rb
@@ -77,6 +77,7 @@ activate :livereload
 #     "Helping"
 #   end
 # end
+set :layout_dir, ''
 
 set :css_dir, 'stylesheets'
 
@@ -89,10 +90,10 @@ set :haml, :format => :html5
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
