35479dd5cca57dbc376088ab7675e2475f92186b
Blake Tidwell <batidwell@gmail.com>
2015-02-07 20:22:27 -0800

    Adds basic Twitter integration

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,5 +1,6 @@
 //= require "plugins"
 // require "vendor/fb"
+//= require "vendor/twitter"
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
